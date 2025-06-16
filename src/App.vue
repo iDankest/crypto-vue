edce1a914eba9fa6b8199e3b16ac5f7ef2ed95a3
Kilian <dankestmirror@hotmail.es>
2025-06-16 12:39:53 +0100

    Las monedas con su array de objetos tipo diccionario

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+
+  const monedas = ref([
+      { codigo: 'USD', texto: 'Dolar de Estados Unidos'},
+      { codigo: 'MXN', texto: 'Peso Mexicano'},
+      { codigo: 'EUR', texto: 'Euro'},
+      { codigo: 'GBP', texto: 'Libra Esterlina'},
+  ])
 </script>
 
 <template>
-  <div>
+  <div class="contenedor">
+    <h1 class="titulo">Cotizador de<span>Cryptomonedas</span></h1>
+    <div class="contenido">
+      <form class="formulario">
+        <div class="campo">
+          <label for="moneda">Moneda:</label>
+          <select id="moneda">
+            <option value="">-- Selecciona una moneda --</option>
+            <option 
+            v-for="moneda in monedas" :key="moneda.codigo"
+            :value="moneda.codigo">{{ moneda.texto }}</option>
+          </select>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
-
-<style scoped>
-
-</style>
